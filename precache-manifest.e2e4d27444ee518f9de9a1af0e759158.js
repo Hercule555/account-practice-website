506078ef00b5f3ee04b807407051ce49a328ee99
Hercule555 <1076297655@qq.com>
2023-08-02 21:50:42 +0800

    修改样式

---
+++ precache-manifest.e2e4d27444ee518f9de9a1af0e759158.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66736a4451f4f8a0d750",
+    "url": "/account-practice-website/css/app.771871cf.css"
+  },
+  {
+    "revision": "8f9ac74da10239dcfbc317134dcb4fa7",
+    "url": "/account-practice-website/img/qrcode.png"
+  },
+  {
+    "revision": "091f3e0682f4cbb621b029b7fca089d8",
+    "url": "/account-practice-website/index.html"
+  },
+  {
+    "revision": "66736a4451f4f8a0d750",
+    "url": "/account-practice-website/js/app.affb1ab1.js"
+  },
+  {
+    "revision": "e26b3f3dc2ba238255af",
+    "url": "/account-practice-website/js/chunk-vendors.9a3f6617.js"
+  },
+  {
+    "revision": "444ade3ec36aa09a87fa5ba355fae244",
+    "url": "/account-practice-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/account-practice-website/robots.txt"
+  }
+]);
